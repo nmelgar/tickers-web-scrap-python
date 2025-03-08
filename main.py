a77f3089b084bbc648d2eafaf6b65c64306fa352
nmelgar <nmelgar16013@gmail.com>
2025-03-08 14:26:52 -0600

    Work to extract data from all the available pages. Data insert to json correctly.

--- main.py
+++ main.py
@@ -3,53 +3,83 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 
 driver = webdriver.Chrome()
-
 web_page_url = "https://stockanalysis.com/stocks/"
 driver.get(web_page_url)
-
 driver.maximize_window()
 
-# table id
-table_xpath = '//*[@id="main-table-wrap"]'
-table = driver.find_element(By.XPATH, table_xpath)
+# close the subscription popup if shown
+# popup_button_xpath = "/html/body/div/div[2]/div/button/svg"
+popup_button_selector = "button[aria-label='Close']"
+try:
+    # Wait for the popup button to be clickable
+    popup_button = WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.CSS_SELECTOR, popup_button_selector))
+    )
+    popup_button.click()
+    print("Popup closed successfully.")
+except TimeoutException:
+    print("Popup button did not become clickable in time.")
+except NoSuchElementException:
+    print("Popup button not found on the page.")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
 
-# list to hold the data
-table_data = []
 
-# rows in the table
-rows = table.find_elements(By.TAG_NAME, "tr")
+def scrap_data(num_pages):
+    """Function to extract the data from the main table"""
+    counter = 1
+    # table id
+    table_xpath = '//*[@id="main-table-wrap"]'
+    table = driver.find_element(By.XPATH, table_xpath)
 
-for row in rows:
-    # all columns in the current row
-    cols = row.find_elements(By.TAG_NAME, "td")
+    # list to hold the data
+    table_data = []
 
-    # create dictionary for current row and change the column name
-    row_data = {}
-    for i, col in enumerate(cols):
-        if i == 0:
-            row_data[f"ticker"] = col.text
-        elif i == 1:
-            row_data["company_name"] = col.text
-        elif i == 2:
-            row_data["investment_type"] = col.text
-        elif i == 3:
-            row_data["investment_amount"] = col.text
+    while counter <= num_pages:
+        # rows in the table
+        rows = table.find_elements(By.TAG_NAME, "tr")
 
-    # append row data to table data list
-    table_data.append(row_data)
+        for row in rows:
+            # all columns in the current row
+            cols = row.find_elements(By.TAG_NAME, "td")
 
-# write the data to the json file
-with open("stock_symbols_data.json", "w") as json_file:
-    json.dump(table_data, json_file, indent=4)
+            # create dictionary for current row and change the column name
+            row_data = {}
+            for i, col in enumerate(cols):
+                if i == 0:
+                    row_data[f"ticker"] = col.text
+                elif i == 1:
+                    row_data["company_name"] = col.text
+                elif i == 2:
+                    row_data["investment_type"] = col.text
+                elif i == 3:
+                    row_data["investment_amount"] = col.text
 
+            # append row data to table data list
+            table_data.append(row_data)
+
+        # click to the next page
+        next_page_xpath = '//*[@id="main"]/div/div/nav/button[2]/span'
+        next_page_button = driver.find_element(By.XPATH, next_page_xpath)
+        next_page_button.click()
+
+        # increment counter
+        counter += 1
+
+    # write the data to the json file
+    with open("stock_symbols_data.json", "w") as json_file:
+        json.dump(table_data, json_file, indent=4)
 
 # define the number of loops the program will perform
 number_pages_xpath = '//*[@id="main"]/div/div/nav/div/span'
 number_pages = driver.find_element(By.XPATH, number_pages_xpath)
 pages_text = number_pages.text
 number_pages_total = int(pages_text[-2:])
-print(number_pages_total)
+total_clicks_next = (number_pages_total - 1)
+
+# call the scrap_Data function
+scrap_data(total_clicks_next)
